483e823b3c04121db3cfe2d8c5e8a3f9ced92048
Eduardo Parra <edue17@gmail.com>
2026-09-20 22:06:37 +0200

    fix: repair onboarding movement syntax

--- src/pages/Onboarding/index.tsx
+++ src/pages/Onboarding/index.tsx
@@ -103,7 +103,7 @@ export const Onboarding = () => {
           babyId,
           sizeId,
           locationId,
-          deviceId:
+          deviceId,
           occurredAt: now,
           recordedAt: now,
         },
