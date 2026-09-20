8858ed9a90abd1cb40783721334a96a8f4ac0dc7
Eduardo Parra <edue17@gmail.com>
2026-09-20 22:02:36 +0200

    fix: restore onboarding movement device id

--- src/pages/Onboarding/index.tsx
+++ src/pages/Onboarding/index.tsx
@@ -91,7 +91,7 @@ export const Onboarding = () => {
           babyId,
           sizeId,
           locationId,
-          deviceId:
+          deviceId,
           occurredAt: now,
           recordedAt: now,
         },
